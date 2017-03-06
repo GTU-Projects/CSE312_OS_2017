3ba124edd17048864363332a966a1451a0138cee
Hasan MEN <hmen.56@gmail.com>
2017-03-06 19:28:29 +0300

    [HW1] teacher source codes added to recent project

--- HW1_8080CPU_Emulation/main.cpp
+++ HW1_8080CPU_Emulation/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include "8080emuCPP.h"
+#include "gtuos.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main (int argc, char**argv)
+{
+	if (argc != 3){
+		std::cerr << "Usage: prog exeFile debugOption\n";
+		exit(1); 
+	}
+	int DEBUG = atoi(argv[2]);
+
+	CPU8080 theCPU;
+	GTUOS	theOS;
+
+	theCPU.ReadFileIntoMemoryAt(argv[1], 0x0000);	
+
+	while (!theCPU.isHalted())
+	{
+		theCPU.Emulate8080p(DEBUG);
+		if(theCPU.isSystemCall())
+			theOS.handleCall(theCPU);
+	}
+	return 0;
 }
+
