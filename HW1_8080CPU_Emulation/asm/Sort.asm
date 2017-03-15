a2b3caa4dd70730bc473c38e8201d40b551af57a
Hasan MEN <hmen.56@gmail.com>
2017-03-15 22:50:07 +0300

    [HW1] Sort.asm implemented. Work good
    GG

--- HW1_8080CPU_Emulation/asm/Sort.asm
+++ HW1_8080CPU_Emulation/asm/Sort.asm
@@ -16,9 +16,9 @@ PRINT_STR	equ 5
 READ_STR	equ 6
 
 	; Position for stack pointer
-stack   equ 0F000h
+stack   equ 0F000H
 
-	org 000H
+	org 0000H
 	jmp begin
 
 	; Start of our Operating System
@@ -37,11 +37,75 @@ GTU_OS:	PUSH D
 	; YOU SHOULD NOT CHANGE ANYTHING ABOVE THIS LINE
 
 
-	ORG 4000H
-    DW 012H,034H,053H,02AH,05BH,06FH, 033H, 021H, 07CH, 0FFH, 0BAH, 0CBH, 0A1H,01AH, 03BH, 0C3H, 04AH, 05DH, 062H, 0A3H, 0B1H, 05CH, 07FH, 0CCH, 0AAH, 034H
+	; my variables to use in searching
+	ORG 8000H
+START:	DW 4,1,0,2
 
+;START:	DW 12H, 34H, 53H, 2AH, 5BH, 6FH, 33H, 21H, 7CH, 0FFH
+;	DW 0BAH, 0CBH, 0A1H, 1AH, 3BH, 0C3H, 4AH, 5DH, 62H, ;0A3H
+;	DW 0B1H, 5CH, 7FH, 0CCH, 0AAH, 34H
+
+	ORG 0700H
+I:	DB 00H ; index of array(address space)
+N:	DB 4 ; number of items to compare
+SIZE	equ 4
+MIN:	DB 03H
+
+
+error_msg:	DW 'error',00AH,00H ; null terminated string
+
+
+
+
+	ORG 000DH
 begin:
 	LXI SP,stack 	; always initialize the stack pointer
 
+WHILE:
+	LDA N ; n = size
+	MVI B,0
+	SUB B
+	JZ EXIT ; n==0
+	DCR A ; n = n-1
+	STA N ;= save N
 
+
+	MVI H,0 ; I=0
+	LXI B,START ; load first element address to BC
+FOR:
+	LDA N ;= load N
+	SUB H ; A = N-H
+	JZ WHILE ; A-I == 0
+
+
+IF:
+	LDAX B ; get a[i]
+	MOV D,A ; D = a[i]
+	INX B
+	INX B
+	LDAX B ; get a[i+1]
+
+	SUB D ; a[i+1] - a[i]
+	JM SWAP ; if A=a[i+1] < D=a[i]
+	JMP CONT_FOR ; go on
+
+SWAP:
+	LDAX B ; a = arr[i+1]
+	MOV E,A ; e = a
+	MOV A,D ; a= d, d=arr[i]
+	STAX B ; arr[i+1] = d
+	DCX B
+	DCX B
+	MOV A,E ; a = e
+	STAX B ; arr[i] = e
+
+CONT_FOR:
+	INR H
+	INX B
+	INX B
+	JMP FOR
+
+
+
+EXIT:
 	hlt		; end program
